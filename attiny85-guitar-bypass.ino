5850dd441438cfd1c1990aa0e03adf5aac320081
Stelian Saracut <stelian@saracut.ro>
2017-08-04 12:27:16 +0300

    initial commit

---
+++ attiny85-guitar-bypass.ino
@@ -0,0 +1,33 @@
+boolean ef = LOW, cb = LOW, lb = LOW;
+
+void setup() {
+  DDRB = 0b10001;
+  PORTB = 0b1001;
+  delay(250);
+  PORTB = 0b1000;
+  delay(250);
+  PORTB = 0b1001;
+  delay(250);
+  PORTB = 0b1000;
+  delay(250);
+  PORTB = 0b1001;
+  
+}
+
+void loop() {
+  cb = debounce(lb);
+  if (lb == LOW && cb == HIGH)
+     ef = !ef;
+  lb = cb;
+  digitalWrite(4,ef);
+}
+
+boolean debounce(boolean last)
+{
+ boolean current = digitalRead(3);
+ if (last != current) {
+   delay(20);
+   current = digitalRead(3);
+ }
+ return current;
+}
