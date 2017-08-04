135714e0f76ecb4ce7fcccbcfe661e569c1cb6e5
Stelian Saracut <stelian@saracut.ro>
2017-08-04 14:29:36 +0300

    small refactoring

--- attiny85-guitar-bypass.ino
+++ attiny85-guitar-bypass.ino
@@ -1,33 +1,39 @@
-boolean ef = LOW, cb = LOW, lb = LOW;
+#define BUTTON_PIN     3
+#define RELAY_PIN      4
+#define STATUS_LED_PIN 0
+
+boolean bypassDisabled = LOW, currentButtonState = LOW, lastButtonState = LOW;
 
 void setup() {
-  DDRB = 0b10001;
-  PORTB = 0b1001;
-  delay(250);
-  PORTB = 0b1000;
-  delay(250);
-  PORTB = 0b1001;
-  delay(250);
-  PORTB = 0b1000;
-  delay(250);
-  PORTB = 0b1001;
-  
+  //pin mode setup
+  pinMode(STATUS_LED_PIN, OUTPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, 1);
+  //blink status led on startup
+  for (byte i = 1; i <= 5; i++)
+  {
+    digitalWrite(STATUS_LED_PIN, (i % 2));
+    delay(250);
+  }
 }
 
 void loop() {
-  cb = debounce(lb);
-  if (lb == LOW && cb == HIGH)
-     ef = !ef;
-  lb = cb;
-  digitalWrite(4,ef);
+  //debounce button
+  currentButtonState = debounce(lastButtonState);
+  //toggle bypass on each button press
+  if (lastButtonState == LOW && currentButtonState == HIGH)
+     bypassDisabled = !bypassDisabled;
+  lastButtonState = currentButtonState;
+  digitalWrite(RELAY_PIN, bypassDisabled);
 }
 
+//button debouncing function
 boolean debounce(boolean last)
 {
- boolean current = digitalRead(3);
+ boolean current = digitalRead(BUTTON_PIN);
  if (last != current) {
    delay(20);
-   current = digitalRead(3);
+   current = digitalRead(BUTTON_PIN);
  }
  return current;
 }
